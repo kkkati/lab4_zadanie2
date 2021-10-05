ab73df3a5b5cdf53bf72cb3349d950428c8885df
Alex <Alex@LAPTOP-GV3B110K>
2021-10-05 15:24:04 +0200

    Добавьте файлы проекта.

---
+++ zadanie2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int plus=0;
+            int minus=0;
+
+            do
+            {
+                n = Convert.ToInt32(Console.ReadLine());
+                if (n > 0)
+                {
+                    plus++;
+                }
+                else
+                {
+                    if (n < 0)
+                    {
+                        minus++;
+                    }
+                }
+            } while (n != 0);
+            if (plus > minus)
+            {
+                Console.WriteLine("Больше положительных чисел, их количество равно {0}", plus);
+            }
+            else
+            {
+                if (minus > plus)
+                {
+                    Console.WriteLine("Больше отрицательных чисел, их количество равно {0}", minus);
+                }
+                else
+                {
+                    if (minus == plus)
+                    {   Console.WriteLine("Количесиво положительных и отрицательных чисел одинаково и равно {0}", minus);
+                        }
+                }
+            }
+
+            //Console.WriteLine(plus);
+            Console.ReadKey();
+
+        }
+    }
+}
